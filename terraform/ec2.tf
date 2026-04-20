6978de911a0476cc2c86e7c698bbfa4a9fc041ea
PrachiBarhate23 <prachibarhate23@gmail.com>
2026-04-20 17:10:36 +0530

    Fix: Coordinate ports 5000/80 and setup Nginx proxy

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -30,6 +30,13 @@ resource "aws_security_group" "payreconcile_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
